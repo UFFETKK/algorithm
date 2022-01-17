50d4021174db154c05284dec59517ef75cd18540
UFFETKK <soyoungan02@naver.com>
2022-01-17 22:19:12 +0900

    20220117

---
+++ level.1/약수의개수와덧셈.js
@@ -0,0 +1,12 @@
+function solution(left, right) {
+  var answer = 0;
+  for (let i = left; i <= right; i++) {
+    let num = [];
+    for (let j = 1; j <= i; j++) {
+      if (i % j === 0) num.push(j);
+    }
+    if (num.length % 2 === 0) answer += i;
+    else answer -= i;
+  }
+  return answer;
+}
