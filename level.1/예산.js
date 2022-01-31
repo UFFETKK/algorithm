6884c43b7c28141753a3ef6bebf185aa99f920d3
UFFETKK <soyoungan02@naver.com>
2022-01-31 19:12:01 +0900

    20220131

---
+++ level.1/예산.js
@@ -0,0 +1,11 @@
+function solution(d, budget) {
+  var answer = 0;
+  let count = 0;
+  d.sort((a, b) => a - b);
+  for (let i = 0; i < d.length; i++) {
+    answer += d[i];
+    count++;
+    if (answer > budget) count--;
+  }
+  return count;
+}
