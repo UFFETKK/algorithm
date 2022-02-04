eb17d3c2d5959678fc9beb8f809a0e2e4a7c8d09
UFFETKK <soyoungan02@naver.com>
2022-02-04 19:18:26 +0900

    20220204

---
+++ level.1/17시저암호.js
@@ -0,0 +1,11 @@
+function solution(s, n) {
+  return s
+    .split("")
+    .map((v) => {
+      if (v === " ") return v;
+      return v.toUpperCase().charCodeAt() + n > 90
+        ? String.fromCharCode(v.charCodeAt() + n - 26)
+        : String.fromCharCode(v.charCodeAt() + n);
+    })
+    .join("");
+}
