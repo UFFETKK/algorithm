01feffdd633a831a20a56d2f24dc8b03e8954e7f
UFFETKK <88070276+UFFETKK@users.noreply.github.com>
2022-03-29 09:37:58 +0900

    Create H-index.js

---
+++ lever.2/H-index.js
@@ -0,0 +1,8 @@
+function solution(citations) {
+    var answer = 0;
+    citations.sort((a,b)=> b-a);
+    for(let i=0; i<citations.length; i++) {
+        if(i+1 <=citations[i]) answer =i+1;
+    }
+    return answer;
+}
