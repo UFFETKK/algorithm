c7d61467cb5a4d96803b296b1aa956a19807351d
UFFETKK <88070276+UFFETKK@users.noreply.github.com>
2022-03-30 10:11:52 +0900

    Create 짝지어제거하기.js

---
+++ lever.2/짝지어제거하기.js
@@ -0,0 +1,11 @@
+function solution(s){
+    let answer = [];
+    for(let i=0; i<s.length; i++) {
+      if(answer[answer.length-1] ==s[i]) {
+        answer.pop();
+      } else {
+        answer.push(s[i])
+      }
+    }
+return answer.length==0?1:0;
+}
