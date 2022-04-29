96b504aa997b3795669db849678abb75fa36fbc4
UFFETKK <88070276+UFFETKK@users.noreply.github.com>
2022-04-29 13:10:40 +0900

    Create 신고결과받기.js

---
+++ level.1/신고결과받기.js
@@ -0,0 +1,24 @@
+function solution(id_list, report, k) {
+    let answer = new Array(id_list.length).fill(0);
+    const new_report = [...new Set(report)]
+    let report_list={}
+    
+    id_list.map((item) => {
+        report_list[item] =[]
+    })
+    
+    report.map((item)=>{
+        const [reportt, reported] = item.split(" ")
+        if (!report_list[reported].includes(reportt)) {
+            report_list[reported].push(reportt)
+        }
+    })
+    
+    for (const item in report_list) {
+        if(report_list[item].length >=k) {
+            report_list[item].map((user)=>{
+                answer[id_list.indexOf(user)] += 1
+            })
+        }
+    }
+    return answer;
